24e15659ac300f773c295c370d830dd6eb56e39a
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-30 21:04:26 +0200

    image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="flex justify-between items-center py-4 px-7 border-b">
       <Link href="/">
         <Image
-          src=""
+          src="https://cdn.pixabay.com/photo/2024/02/23/11/50/woman-8591903_1280.jpg"
           alt="logo"
           className="h-[35px] w-[35px]"
           width="50"
