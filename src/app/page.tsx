92e9aba9d4fbe58815aab4d9cad78d5fead3fe48
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-31 12:30:44 +0300

    client vs server component && server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
-export default function Home() {
+import PostsList from '@/components/postsList'
+import AddPostForm from '@/components/add-post-form';
+import prisma from '@/lib/db';
+
+export default async function Home() {
+  // const response = await fetch(
+  //   `https://dummyjson.com/posts?limit=3`,
+  //   {
+  //     next: {
+  //       revalidate: 3600,
+  //     },
+  //   }
+  // );
+  // const data = await response.json();
+
+  const posts = await prisma.post.findMany();
+
   return (
     <main className="text-center pt-32 px-5">
+      <AddPostForm />
+
       <h1 className="text-4xl md:text-5xl font-bold mb-5">
         Welcome to my blog
       </h1>
-      <p className="max-w-[750px] mx-auto leading-8">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
-        repellendus iste error nihil facilis dignissimos. Distinctio explicabo
-        architecto amet voluptatem quasi iste, sed labore adipisci voluptates
-        aliquam? Laudantium dolores libero totam sunt modi dolor illum corrupti
-        ducimus labore perferendis sint praesentium fugit accusantium quam,
-        delectus ut recusandae possimus facilis?
-      </p>
+      
+      <PostsList posts={posts} />
     </main>
   );
 }
